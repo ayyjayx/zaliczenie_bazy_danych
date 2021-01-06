a1e75b84ddf6da49f8b48b0a5e02fb3bc2a11e87
ayyjayx <73549348+ayyjayx@users.noreply.github.com>
2021-01-06 17:52:59 +0100

    Update procedura.sql

--- zaliczenie_bazy_danych/procedura.sql
+++ zaliczenie_bazy_danych/procedura.sql
@@ -3,5 +3,5 @@ CREATE PROCEDURE najstarsza_ksiazka (OUT najstarsza YEAR)
 BEGIN
 SELECT MIN(rok_wydania) INTO najstarsza FROM ksiazka;
 END;
-CALL najstarsza_ksiazka(@M); $$
-SELECT @M;
+CALL najstarsza_ksiazka(@najstarsza); $$
+SELECT @najstarsza;
